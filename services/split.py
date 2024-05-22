12ac6c38c6364f240d3e5c8c87196fc1b7cf6907
Matheus Santos <matheusps0012@gmail.com>
2024-05-22 17:56:33 -0300

    fix: pdf split

--- services/split.py
+++ services/split.py
@@ -4,16 +4,16 @@ import time
 import os
 
 def handle(file):
-    writer = PdfWriter()
     splited_files = []
 
     if file.endswith(".pdf"):
         pdf = PdfReader(file)
 
         for page in range(len(pdf.pages)):
+            writer = PdfWriter()
             writer.add_page(pdf.pages[page])
             output_filename = f"{os.curdir}/result/{int(time.time())}_page_{page + 1}.pdf"
-            splited_files.append(f"{os.curdir}/result/{int(time.time())}_page_{page + 1}.pdf")
+            splited_files.append(output_filename)
 
             with open(output_filename, "wb") as out:
                 writer.write(out)
