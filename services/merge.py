5f765e9e6d9d3ae6f0f0dd17795cf9ade02aba37
Matheus Santos <matheusps0012@gmail.com>
2024-05-22 18:16:10 -0300

    fix: pdf merge

--- services/merge.py
+++ services/merge.py
@@ -1,18 +1,23 @@
-from PyPDF2 import PdfWriter
+from PyPDF2 import PdfWriter, PdfReader
 import time
 import os
 
 
 def handle(files):
-    writer = PdfWriter()
+    pdf_writer = PdfWriter()
     for file in files:
         if file.endswith(".pdf"):
-            writer.append(os.path.join("uploads", file))
+            pdf_reader = PdfReader(os.path.join("uploads", file))
+            for page_num in range(len(pdf_reader.pages)):
+                    page = pdf_reader.pages[page_num]
+                    pdf_writer.add_page(page)
+    
+    filename = f"{os.curdir}/result/merged_{int(time.time())}.pdf"
+    print(filename)
+    
+    with open(filename, "wb") as output_file:
+        pdf_writer.write(output_file)
 
-    filename = f"mergered_{int(time.time())}.pdf"
-    mergered_file_path = f"result/{filename}"
+    pdf_writer.close()
 
-    writer.write(mergered_file_path)
-    writer.close()
-
-    return {"path": mergered_file_path, "filename": filename}
+    return {"path": filename, "filename": f"merged_{int(time.time())}.pdf"}
